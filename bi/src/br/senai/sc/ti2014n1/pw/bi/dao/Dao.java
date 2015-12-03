e4836a11c3e6796807bf49fee9553b95d3c56598
Eder Soares <edetec@gmail.com>
2015-12-03 19:30:22 -0200

    Implementados os parâmetros de configuração do banco de dados

--- bi/src/br/senai/sc/ti2014n1/pw/bi/dao/Dao.java
+++ bi/src/br/senai/sc/ti2014n1/pw/bi/dao/Dao.java
@@ -3,6 +3,9 @@ package br.senai.sc.ti2014n1.pw.bi.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
 public abstract class Dao {
 
 	private Connection conn;
@@ -11,8 +14,15 @@ public abstract class Dao {
 		if (conn == null) {
 			try {
 				Class.forName("com.mysql.jdbc.Driver");
+				ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+				
+				String servidor = externalContext.getInitParameter("servidor");
+				String banco = externalContext.getInitParameter("banco");
+				String usuario = externalContext.getInitParameter("usuario");
+				String senha = externalContext.getInitParameter("senha");
+				
 				conn = DriverManager.getConnection(
-						"jdbc:mysql://localhost/bi_db", "root", "root");
+						"jdbc:mysql://" + servidor +"/" + banco, usuario, senha);
 				System.out.println("Conectou");
 			} catch (Exception e) {
 				e.printStackTrace();
